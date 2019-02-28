0fc3e71585e9cf23840ba923dd88c7f771aac45a
Vasiliy Dumanov <vasiliy.dumanov@gmail.com>
2019-02-28 18:42:01 +0300

    Initial Commit

---
+++ SwiftMLP/SwiftMLP.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMLP.h
+//  SwiftMLP
+//
+//  Created by Vasiliy Dumanov on 2/28/19.
+//  Copyright © 2019 Distillery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftMLP.
+FOUNDATION_EXPORT double SwiftMLPVersionNumber;
+
+//! Project version string for SwiftMLP.
+FOUNDATION_EXPORT const unsigned char SwiftMLPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMLP/PublicHeader.h>
+
+
